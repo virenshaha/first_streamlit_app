17841d773025a25f4f88cb4ab160d64ef9e4867f
Viren Shaha <120490679+virenshaha@users.noreply.github.com>
2022-12-14 16:37:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
+import streamlit
+
+streamlit.title('Breakfast Favorites')
+streamlit.text('🥧Omega 3 & Blueberry oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
 import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)  
