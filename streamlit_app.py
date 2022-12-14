3c306ab960500bb6fe33bf7dde2f02c885ed4a41
Viren Shaha <120490679+virenshaha@users.noreply.github.com>
2022-12-14 15:55:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.header('🥧Breakfast Favorites')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥧Omega 3 & Blueberry oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
