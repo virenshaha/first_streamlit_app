0f963ce93fd8242ed3db242ef5533acdf898d463
Viren Shaha <120490679+virenshaha@users.noreply.github.com>
2022-12-14 15:04:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My parents new healthy dinner')
+streamlit.title('My parents new healthy dinner
+                
+                Breakfast Menu
+                
+                ')
