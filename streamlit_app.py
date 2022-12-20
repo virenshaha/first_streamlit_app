cfbcfa163f99f479d14bde6e5cbbb0e413f83a7e
Viren Shaha <120490679+virenshaha@users.noreply.github.com>
2022-12-20 14:58:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib import URLError 
+from urllib.error import URLError 
 
 streamlit.title('Breakfast Favorites')
 streamlit.text('🥧Omega 3 & Blueberry oatmeal')
